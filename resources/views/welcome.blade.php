5c9bc920d15e694aa7e8e59264361675e8415011
suthisaji <asihtus10@gmail.com>
2017-05-05 13:48:53 +0700

    change welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Animal-AID
+                    --Animal-AID--
                 </div>
 
                 <div class="links">
