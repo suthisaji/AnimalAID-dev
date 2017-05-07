9180c7c6cf990ee5e1af8ac29efd8ec1edffa8a9
suthisaji <asihtus10@gmail.com>
2017-05-07 15:22:04 +0700

    testnews7

--- resources/views/news2.blade.php
+++ resources/views/news2.blade.php
@@ -103,33 +103,7 @@ body{
 
 <!--check login yet-->
 
-    <!-- Authentication Links -->
-    @if (Auth::guest())
-        <li class="fl"><a href="{{ route('login') }}">เข้าสู่ระบบ</a></li>
-        <li class="fl"><a href="{{ route('register') }}">สมัครสมาชิก</a></li>
-    @else
-        <li class="dropdown fl">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                {{ Auth::user()->name }} <span class="caret"></span>
-            </a>
 
-            <ul class="dropdown-menu" role="menu">
-                <li class="fl">
-                    <a href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                 document.getElementById('logout-form').submit();">
-                        ออกจากระบบ
-                    </a>
-
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
-                </li>
-            </ul>
-        </li>
-    @endif
-</ul>
-<!--end check-->
 
 
 
