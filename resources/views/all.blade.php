9daab7404a169702dc32a377c45590bdfcd11870
suthisaji <asihtus10@gmail.com>
2017-08-16 16:50:54 +0700

    testmodal4

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -29,7 +29,6 @@
     <!-- Custom CSS -->
     <!--old - - - <link href="https://blackrockdigital.github.io/startbootstrap-shop-homepage/css/shop-homepage.css" rel="stylesheet">-->
 
-    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
     <!-- Custom Fonts -->
             <link href="https://fonts.googleapis.com/css?family=Athiti" rel="stylesheet">
@@ -617,4 +616,5 @@ body{
    </div>
    <!-- /.container -->
     </body>
+    <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
 </html>
