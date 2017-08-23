0ea80cdeedb05546e9764f4ad458a615cd208a9c
suthisaji <asihtus10@gmail.com>
2017-08-23 18:44:15 +0700

    responmodal

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -63,7 +63,7 @@
         .box1{
            text-align: left;
         }
-        
+
 
     </style>
 
@@ -203,7 +203,7 @@
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                             <h4 class="modal-title" id="myModalLabel" > {{$animal->join_donationType->do_typeName}} to  {{$animal->animal_type}}</h4>
                                           </div>
-                                          <div class="modal-body thumbnailjam ">
+                                          <div class="modal-body thumbnail  ">
                                              <img src="{{url('/images/'.$animal->animal_picture)}}" alt="" width="566" height="390">
                                             ประเภทของการขอรับบริจาค:
                                               @if($animal->join_donationType->do_typeName=='Donation Money')
@@ -456,7 +456,7 @@
                                   <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
                                   <h4 class="modal-title" id="myModalLabel" > {{$animal->join_donationType->do_typeName}} to  {{$animal->animal_type}}</h4>
                                 </div>
-                                <div class="modal-body thumbnailjam">
+                                <div class="modal-body thumbnail">
                                    <img src="{{url('/images/'.$animal->animal_picture)}}" alt="" width="200" height="500">
                                   ประเภทของการขอบริจาค:
                                    @if($animal->join_donationType->do_typeName=='Donation Money')
