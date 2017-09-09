63619b03c37fc949d62fb7af0998ada1858f60d2
suthisaji <asihtus10@gmail.com>
2017-09-09 19:38:49 +0700

    trytokeppdonor5

--- app/Http/Controllers/DonateController.php
+++ app/Http/Controllers/DonateController.php
@@ -5,24 +5,9 @@ require_once dirname(__FILE__, 3).'/omise/lib/Omise.php';
 use Illuminate\Http\Request;
 use Response;
 use Nexmo\Laravel\Facade\Nexmo;
-use App\Donation;
-use App\Repositories\DonationRepositoryInterface;
-use App\User;
-use Auth;
-use DB;
 
 class DonateController extends Controller
 {
-  protected $DonationRepository;
-
- function __construct(DonationRepositoryInterface $DonationRepository,UserRepositoryInterface $UserRepository){
-     $this->DonationRepository = $DonationRepository;
-       $this->UserRepository = $UserRepository;
- }
-//ข้างบนจะเรียกใช้ repo ที่สร้าง
-
-
- /*รับจาก omise*/
     function charge(Request $request){
         $data = $request->json()->all();
         $amount = $data['amount'];
@@ -66,22 +51,19 @@ class DonateController extends Controller
                     'text' => 'ขอขอบคุณ '.$name.' '.$sname.' ที่บริจาคเงินจำนวน '.$amount.' บาท ให้แก่ ANIMAL-AID',
                     'type' => 'unicode'
                 ]);*/
-          //    $result = $this->DonationRepository->addDonation($name,$sname,$email,$tel,$bank,$amount);
+              $result = $this->DonationRepository->addDonation($name,$sname,$email,$tel,$bank,$amount);
 
-            //    if($result){
-            //        echo "success to add donationdata";
-              //  }else{
-              //      echo "Failed to add donation";
-              //  }
-
-
+                if($result){
+                    echo "success to add donationdata";
+                }else{
+                    echo "Failed to add donation";
+                }
+                
                 return Response::json([
                     'statusCode' => 200,
                     'statusMessage' => 'Success',
                     'payload' => $amount
                     ], 200);
-
-
 
             }else{ //ถ้าจ่ายไม่สำเร็จ
 
