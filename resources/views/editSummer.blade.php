c73f478b5c2960c2d15fad01fb4bf361949ad8c9
suthisaji <asihtus10@gmail.com>
2017-09-11 00:14:08 +0700

    navOfeditSummerPage

--- resources/views/editSummer.blade.php
+++ resources/views/editSummer.blade.php
@@ -2,7 +2,8 @@
 <html>
   <head>
     <meta charset="utf-8">
-
+    <!-- CSRF Token -->
+       <meta name="csrf-token" content="lTGlgWKoqayN8ejijUYGiNOqRPDyRt58eg8QpU7w">
     <!-- include libraries(jQuery, bootstrap) -->
     <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
       <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
@@ -10,8 +11,83 @@
       <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.css" rel="stylesheet">
       <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
     <title>summer Note</title>
+    <!-- Styles -->
+    <link href="https://animal-aid.me/css/app.css" rel="stylesheet">
+
+    <!-- Scripts -->
+    <script>
+        window.Laravel = {"csrfToken":"lTGlgWKoqayN8ejijUYGiNOqRPDyRt58eg8QpU7w"};
+    </script>
+    <style>
+
+    </style>
   </head>
   <body>
+    <div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+           <div class="container">
+               <div class="navbar-header ">
+
+                   <!-- Collapsed Hamburger -->
+                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                       <span class="sr-only">Toggle Navigation</span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                       <span class="icon-bar"></span>
+                   </button>
+
+                   <!-- Branding Image-->
+                   <a class="navbar-brand" href="https://animal-aid.me/all">
+
+               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Animal-AID
+                   </a>
+
+                     <a  class="navbar-brand" href="../add">&nbsp;&nbsp;&nbsp; เพิ่มการขอรับบริจาค &nbsp;&nbsp;</a> &nbsp;
+                     <a class="navbar-brand" href="../animal">  รายชื่อสัตว์&nbsp;&nbsp; </a>&nbsp;
+                     <a  class="navbar-brand" href="../addNews">   เพิ่มข่าวและกิจกรรม&nbsp;&nbsp;</a>&nbsp;
+
+                 <!--    <a class="navbar-brand"  href="../n">   ข่าวทั้งหมด &nbsp;&nbsp;</a>&nbsp;-->
+                     <a class="navbar-brand" href="../checkAdoption"> ตรวจสอบการขอรับเลี้ยงสัตว์ :<span style="color:red"> 0</span><a>&nbsp;
+
+
+
+
+                   <a href="#"></a>
+               </div>
+
+               <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                   <!-- Left Side Of Navbar -->
+
+
+                   <!-- Right Side Of Navbar -->
+                   <ul class="nav navbar-nav navbar-right">
+                       <!-- Authentication Links -->
+                                                   <li class="dropdown">
+                               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                   {{ Auth::user()->name }}<span class="caret"></span>
+                               </a>
+
+                                       <ul class="dropdown-menu" role="menu">
+                                                 <li>
+                                                   <a   href="../adminProfile"> ข้อมูลส่วนตัว &nbsp;&nbsp; </a>
+                                                 </li>
+                                                     <li>
+                                                         <a href="https://animal-aid.me/logout"
+                                                           onclick="event.preventDefault();
+                                                           document.getElementById('logout-form').submit();">
+                                                           ออกจากระบบ
+                                                           </a>
+
+                                                           <form id="logout-form" action="https://animal-aid.me/logout" method="POST" style="display: none;">
+                                                             <input type="hidden" name="_token" value="lTGlgWKoqayN8ejijUYGiNOqRPDyRt58eg8QpU7w">
+                                                           </form>
+                                                         </li>
+                                       </ul>
+                           </li>
+                                           </ul>
+               </div>
+           </div>
+       </nav>
 
     <div class="container">
       <div class="panel panel-default">
@@ -49,6 +125,7 @@
           </div>
         </div>
       </div>
+    </div>
     <script type="text/javascript">
          $(document).ready(function(){
            $('#summernote').summernote({
