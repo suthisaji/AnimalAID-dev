c33ecc58bcbf9ce1b223953acb32059e1841bd47
suthisaji <asihtus10@gmail.com>
2017-09-22 13:52:27 +0700

    pic try3

--- resources/views/ADEFhome.blade.php
+++ resources/views/ADEFhome.blade.php
@@ -298,7 +298,8 @@
         <script src="js/jquery.js"></script>
 
         <!-- Bootstrap Core JavaScript -->
-        <script src="js/bootstrap.min.js"></script>
+        <!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
     </body>
 
