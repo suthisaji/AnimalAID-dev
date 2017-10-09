221de926316d8aa9f2e53fce88fcf4a370dc38a7
suthisaji <asihtus10@gmail.com>
2017-10-10 00:23:04 +0700

    monneyAll

--- resources/views/allMoney.blade.php
+++ resources/views/allMoney.blade.php
@@ -164,11 +164,64 @@
   background-image: linear-gradient(to bottom,	#E5E4E2 0,	#B6B6B4 100%);
 }
 /*จบเนป*/
+/*bank*/
+.donate-box-header{
+  font-weight: bold;
+  display: inline;
+}
+/*.left-tab{
+  float: left;
+}
+
+.right-tab{
+  float: right;
+}*/
+
+
+.bank-list-item{
+  list-style: none;
+  overflow: hidden;
+  padding: 1rem 0;
+  border-bottom: 1px solid #ebebeb;
+  cursor: pointer;
+}
+
+.paytype{
+  width: 7rem;
+  float: left;
+  line-height: 2.4rem;
+}
+.paytype > img{
+  width: 100%;
+}
+
+.payname{
+  color: #000;
+  float: left;
+  margin-left: 1rem;
+}
+
+.selected-bank{
+  display: none;
+}
+
+.selected-bank h3, img {
+    display: block;
+    margin: 0 auto;
+    text-align: center;
+    line-height: 5rem;
+}
+
+.selected-bank > img{
+  width: 7rem;
+}
+/*endBank*/
 
     </style>
 
   </head>
   <body>
+
     <!-- Navigation -->
    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">  <a  class="navbar-brand" href="all">   <img src="images/S__1261612.jpg" width="60" height="60" class="d-inline-block align-top" alt=""></a>
 
@@ -327,41 +380,128 @@
                </div> -->
 
                <div class="container">
+                 <!-- Page Heading -->
+                 <div class="row">
+                     <div class="col-lg-12">
+                         <h1 class="page-header">ช่องทางการบริจาคเงิน</h1>
+                     </div>
+                 </div
+                 <!-- /.row -->
 
+                 <div class="row text-center">
+
+                     <div class="col-lg-12">
+                       <h2>บริจาคเงินผ่านทางบัตรเครดิต</h2>
+                       <h3>ชื่อบัญชี มูลนิธิเพื่อสัตว์ประสบภัยและเหตุฉุกเฉิน</h3>
+                       <h4>ผ่านบัญชีออมทรัพย์ ธนาคารกรุงเทพ สาขา บิ๊กซีอยุธยา</h4>
+                       <h2>เลขที่บัญชี 790-020373-3</h2>
+                       <br>
+
+                     </div>
+                   </div>
+
+                   <div class="row">
+                     <!-- DONATE BOX -->
+                       <div class="col-md-6 col-md-offset-3">
+                         <div class="donate-box">
+                             <div class="donate-box-header">
+                               <div class="left-tab">
+                                 ธนาคารออนไลน์
+                               </div>
+                               <div class="right-tab">
+                                 บัตรเครดิต
+                               </div>
+                             </div>
+                             <div class="donate-box-content">
+                               <div class="selected-bank">
+                                 <input type="hidden" id="bankcode" value="">
+                                 <div class="row">
+                                   <div class="col-md-12 getBankSelected">
+                                     <h3></h3>
+                                     <img src="" alt="" />
+                                   </div>
+                                 </div>
+                                 <div class="row">
+                                   <div class="row">
+                                     <div class="col-md-6">
+                                       <label for="">ชื่อ</label>
+                                       <input type="text" class="form-control" id="name" placeholder="ชื่อ"/>
+                                     </div>
+                                     <div class="col-md-6">
+                                       <label for="">นามสกุล</label>
+                                       <input type="text" class="form-control" id="sname" placeholder="นามสกุล"/>
+                                     </div>
+                                   </div>
+                                   <div class="row">
+                                     <div class="col-md-12">
+                                       <label for="">เบอร์โทรศัพท์ (กรอกตามความจริงเพื่อรับ SMS ยืนยันการบริจาคเงิน)</label>
+                                       <input type="tel" class="form-control" id="tel" placeholder="โทรศัพท์"/>
+                                     </div>
+                                   </div>
+                                   <div class="row">
+                                     <div class="col-md-12">
+                                       <label for="">จำนวนเงิน</label>
+                                       <input type="email" class="form-control" id="amount" placeholder="จำนวนเงินต้องมากกว่า 20 บาท"/>
+                                     </div>
+                                   </div>
+                                   <div class="row">
+                                     <div class="col-md-6 col-md-offset-3 text-center" style="padding: 1rem;">
+                                       <button id="submit-donate-btn" type="button" class="btn btn-success">ยืนยัน</button>
+                                       <button id="cancel-donate-btn" type="button" class="btn btn-danger">ยกเลิก</button>
+                                     </div>
+                                   </div>
+                                 </div>
+                               </div>
+
+                               <ul class="bank-list">
+                                 <li class="bank-list-item">
+                                   <div class="paytype">
+                                       <img src="{{url('/images/logos/ayud.png')}}" alt="" />
+                                   </div>
+                                   <div class="payname">ธนาคารกรุงศรี</div>
+                                 </li>
+                                 <li class="bank-list-item">
+                                   <div class="paytype">
+                                       <img src="{{url('/images/logos/bkk.png')}}" alt="" />
+                                   </div>
+                                   <div class="payname">ธนาคารกรุงเทพ</div>
+                                 </li>
+                                 <li class="bank-list-item">
+                                   <div class="paytype">
+                                       <img src="{{url('/images/logos/ktb.png')}}" alt="" />
+                                   </div>
+                                   <div class="payname">ธนาคารกรุงไทย</div>
+                                 </li>
+                                 <li class="bank-list-item">
+                                   <div class="paytype">
+                                       <img src="{{url('/images/logos/scb.png')}}" alt="" />
+                                   </div>
+                                   <div class="payname">ธนาคารไทยพาณิชย์</div>
+                                 </li>
+                               </ul>
+                             </div>
+                         </div>
+                       </div>
+                     <!-- DONATE BOX -->
+                   </div>
+                   {{--
+                      <div class="col-lg-12" style="background-color: #EFFBFB;padding:30px 40px;">
+                         <h3><b style="font-size: 32px;">Omise </b> &nbsp;เป็น Payment Gateway<h3>
+                       <h2>บริจาคเงินผ่านทางบัตรเครดิต</h2>
+                       <h3>เราได้เลือก Omise เป็นช่องทางการในการบริจาคเงินออนไลน์</h3>
+                       <h3>ผู้ใช้จะปลอดภัยจากการปลอมแปลง หรือเก็บข้อมูลบัตรเครดิต เพราะมีเทคโนโลยีตรวจสอบข้อมูลว่า เป็นข้อมูลจริงหรือไม่</h3>
+                       <h3>ทำให้ไม่มีเรื่องการกรอกข้อมูลปลอมได้ ระบบของเราไม่จำเป็นต้องเก็บข้อมูลบัตรเครดิตของผู้ใช้ไว้</h3>
+                       <h3>เพราะ Token จะเป็นตัวแทนข้อมูลบัตรเครดิตของผู้ใช้ที่ถูกเข้ารหัสไว้แล้ว </h3> --}}
+
+
+
+         <!-- data-key="pkey_test_57gpwuk3sm7mirumtsx" -->
+<br><br><br><h1>สัตว์ที่รอการช่วยเหลือ</h1><br><br>
                    <div class="row">
 
                        <div class="col-md-9">
 
-                           <div class="row carousel-holder">
 
-                               <div class="col-md-12">
-                                   <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-                                       <ol class="carousel-indicators">
-                                           <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-                                           <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-                                           <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-                                       </ol>
-                                       <div class="carousel-inner">
-                                           <div class="item active">
-                                               <img class="slide-image" src="\images\animal.png" alt="">
-                                           </div>
-                                           <div class="item">
-                                               <img class="slide-image" src="\images\S__1261615.jpg" alt="">
-                                           </div>
-                                           <div class="item">
-                                               <img class="slide-image" src="\images\puppy.jpg" alt="">
-                                           </div>
-                                       </div>
-                                       <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
-                                           <span class="glyphicon glyphicon-chevron-left"></span>
-                                       </a>
-                                       <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
-                                           <span class="glyphicon glyphicon-chevron-right"></span>
-                                       </a>
-                                   </div>
-                               </div>
-
-                           </div>
 
 
                <div class="row">
@@ -485,8 +625,7 @@
            </div>
            <div class="col-md-3 ro">
 
-               <a  href="donate">   <img onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0" src="\images\icondonate.png" alt="donateIcon" width="240" height="160"></a>
-               <p class="lead">รายชื่อผู้บริจาค</p>
+
                 @php($i=0)
                @if($i<11)
                @foreach($donor as $donor)
@@ -561,7 +700,81 @@
        x.style.height = "160px";
        x.style.width = "240px";
    }
+   </body>
+   <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.16.2/axios.min.js"></script>
+   <script>
+   $(document).ready(function()
+   {
+     $('ul.bank-list li').click(function(e)
+       {
+        const bankName = $(this).find("div.payname").text();
+        let bankCode = '';
+        let bankLogo = '';
+        switch (bankName) {
+          case 'ธนาคารกรุงศรี':
+            bankCode = 'internet_banking_bay'
+            bankLogo = '{{url('/images/logos/ayud.png')}}'
+            break;
+          case 'ธนาคารกรุงเทพ':
+            bankCode = 'internet_banking_bbl'
+            bankLogo = '{{url('/images/logos/bkk.png')}}'
+            break;
+          case 'ธนาคารกรุงไทย':
+            bankCode = 'internet_banking_ktb'
+            bankLogo = '{{url('/images/logos/ktb.png')}}'
+            break;
+          case 'ธนาคารไทยพาณิชย์':
+            bankCode = 'internet_banking_scb'
+            bankLogo = '{{url('/images/logos/scb.png')}}'
+            break;
+          default:
+             bankCode = null
+        }
+        $('.bank-list').css('display', 'none')
+        $('#bankcode').val(bankCode)
+        $('.getBankSelected > h3').text(bankName);
+        $('.getBankSelected > img').attr('src', bankLogo);
+        $('.selected-bank').css('display', 'block')
+        console.log('bankLogo is ', bankLogo)
+       });
+
+       $('#cancel-donate-btn').click(function(e){
+         e.preventDefault();
+         console.log('eeee')
+         $('.selected-bank').css('display', 'none')
+         $('.bank-list').css('display', 'block')
+       })
+
+       $('#submit-donate-btn').click(function(e){
+         const name = $('#name').val()
+         const surname = $('#sname').val()
+         const tel = $('#tel').val()
+         const amount = $('#amount').val()*100
+         const bankCode = $('#bankcode').val()
+         const data = {
+           amount: amount,
+           name: name,
+           sname: surname,
+           tel: tel,
+           bank: bankCode
+         }
+         fetch("{{url('/charge')}}", {
+           method: 'POST',
+           body: JSON.stringify(data)
+         }).then(res => {
+           return res.json();
+         }).then(json => {
+           return window.location.href = json.url
+         }).catch(err => {
+           console.log('error ',err)
+         })
+       })
+
+   })
+
+
    </script>
+
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
