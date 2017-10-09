26e03437af3d85f5fbeda3ca40128745b5696513
Your Name <you@example.com>
2017-10-10 02:29:50 +0700

    AdminAgain

--- resources/views/editSummer.blade.php
+++ resources/views/editSummer.blade.php
@@ -1,95 +1,108 @@
 <!DOCTYPE html>
 <html>
-  <head>
+<head>
+    <!-- Required meta tags -->
+    <script src="https://cdn.omise.co/card.js" charset="utf-9"></script>
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- include libraries(jQuery, bootstrap) -->
-    <link href="https://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-      <script src="https://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.css" rel="stylesheet">
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.8/summernote.js"></script>
-    <title>แอดมินแก้ไขคำถาม</title>
-    <!-- Styles -->
-    <link href="https://animal-aid.me/css/app.css" rel="stylesheet">
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {"csrfToken":"lTGlgWKoqayN8ejijUYGiNOqRPDyRt58eg8QpU7w"};
-    </script>
-    <style>
-    .navbar>.container-fluid .navbar-brand, .navbar>.container .navbar-brand {
-        margin-left: -34px;
-    }
+    <title>AdmineditAsk</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" link href="{{url('css/bootstrap.css')}}">
+    <link rel="stylesheet" link href="{{url('css/bootstrap.min.css')}}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{url('/css/shop-homepage.css')}}"/>
+    <link rel="stylesheet" href="{{url('/css/index.css')}}"/>
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <!-- <script src="https://blackrockdigital.github.io/startbootstrap-shop-homepage/js/bootstrap.min.js"></script> -->
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Athiti">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+    <script>window.Laravel = {"csrfToken":"lTGlgWKoqayN8ejijUYGiNOqRPDyRt58eg8QpU7w"};</script>
+
+    {{Html::style('css/shop-homepage.css')}}
+
+    <style type="text/css">
+      li{
+        font-size: 15px;
+      }
+
+      body{
+        font-size: 16px;
+      }
+
+      .fl{
+        font-size: 16px;
+      }
+
+      .owner{
+        width: 40%
+      }
+
     </style>
-  </head>
-  <body>
-    <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-           <div class="container">
-               <div class="navbar-header ">
+</head>
 
-                   <!-- Collapsed Hamburger -->
-                   <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                       <span class="sr-only">Toggle Navigation</span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                       <span class="icon-bar"></span>
-                   </button>
+<body>
+  <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <div class="container">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand"  style="font-size:20px;" href="all">Animals A-I-D </a>
+    </div>
 
-                   <!-- Branding Image-->
-                   <a class="navbar-brand" href="https://animal-aid.me/all">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+            <li><a href="../add">เพิ่มการขอรับบริจาค</a></li>
+            <li><a href="../animal">รายชื่อสัตว์</a></li>
+            <li><a href="../addNews">เพิ่มข่าวและกิจกรรม</a></li>
+            <li><a href="../checkAdoption">ตรวจสอบการขอรับเลี้ยงสัตว์: <span style="color:red"> 0 </span></a></li>
+            <li class="active"><a href="../admin">ตอบปัญหา: <span style="color:red">{{DB::table('blogs')->where('status','answered')->count()}}</span>/{{DB::table('blogs')->count()}}</a></li>
+        </ul>
 
-               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Animal-AID
-                   </a>
+        <ul class="nav navbar-nav navbar-right">
+          <!-- Left Side Of Navbar -->
 
-                     <a  class="navbar-brand" href="../add">&nbsp;&nbsp;&nbsp; เพิ่มการขอรับบริจาค &nbsp;&nbsp;</a> &nbsp;
-                     <a class="navbar-brand" href="../animal">  รายชื่อสัตว์&nbsp;&nbsp; </a>&nbsp;
-                     <a  class="navbar-brand" href="../addNews">   เพิ่มข่าวและกิจกรรม&nbsp;&nbsp;</a>&nbsp;
-
-                 <!--    <a class="navbar-brand"  href="../n">   ข่าวทั้งหมด &nbsp;&nbsp;</a>&nbsp;-->
-                     <a class="navbar-brand" href="../checkAdoption"> ตรวจสอบการขอรับเลี้ยงสัตว์ :<span style="color:red"> 0</span><a>&nbsp;
-         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a  class="navbar-brand" href="../admin">     &nbsp;&nbsp; ตอบปัญหา:<span style="color:red"> {{DB::table('blogs')->where('status','answered')->count()}}</span>/{{DB::table('blogs')->count()}}</a>
-
-
-
-
-               </div>
-
-               <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                   <!-- Left Side Of Navbar -->
-
-
-                   <!-- Right Side Of Navbar -->
-                   <ul class="nav navbar-nav navbar-right">
-                       <!-- Authentication Links -->
-                                                   <li class="dropdown">
-                               <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                   {{ Auth::user()->name }}<span class="caret"></span>
-                               </a>
-
-                                       <ul class="dropdown-menu" role="menu">
-                                                 <li>
-                                                   <a   href="../adminProfile"> ข้อมูลส่วนตัว &nbsp;&nbsp; </a>
-                                                 </li>
-                                                     <li>
-                                                         <a href="{{route('logout')}}"
-                                                           onclick="event.preventDefault();
-                                                           document.getElementById('logout-form').submit();">
-                                                           ออกจากระบบ
-                                                           </a>
-
-                                                           <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">
-                                                             {{csrf_field()}}
-                                                           </form>
-                                                           </form>
-                                                         </li>
-                                       </ul>
-                           </li>
-                                           </ul>
-               </div>
-           </div>
-       </nav>
+          <!-- Right Side Of Navbar -->
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }}<span class="caret"></span></a>
+            <ul class="dropdown-menu" role="menu">
+              <li><a href="../adminProfile">ข้อมูลส่วนตัว</a></li>
+              <li><a href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">ออกจากระบบ</a>
+                <form id="logout-form" action="{{route('logout')}}" method="POST" style="display: none;">{{csrf_field()}}</form></li>
+            </ul>
+        </ul>
+    </div>
+    </div>
+  </nav>
 
     <div class="container">
       <div class="panel panel-default">
@@ -101,7 +114,7 @@
             {{ Form::token() }}
           <div class="form-group">
             <label for="topic">ชื่อผู้ถาม</label>
-              <input type="hidden" name="id" value="{{$data->id}}"> คุณ: 
+              <input type="hidden" name="id" value="{{$data->id}}"> คุณ:
             <input type="text" name="name" id="name" class="form-control" value=" {{$data->name}}">
           </div>
           <div class="form-group">
