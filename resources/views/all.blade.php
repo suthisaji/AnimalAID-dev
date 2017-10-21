2e327a7f91e45e6e068729fd1ed573dfed8f89b8
suthisaji <asihtus10@gmail.com>
2017-10-21 21:25:49 +0700

    backblood2

--- resources/views/all.blade.php
+++ resources/views/all.blade.php
@@ -486,7 +486,7 @@ transform: skew(-135deg) translateX(0%);
 
                <div class="row">
 
-                 <!-- Related Projects Row -->
+                 <!-- Related Projects Row  -->
                    <div class="col-md-12">
                      <h2 class="my-4">Product</h2>
 
