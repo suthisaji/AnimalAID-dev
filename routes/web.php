6e78c53a05ab742204fae75977479e889ef42d04
suthisaji <asihtus10@gmail.com>
2017-10-22 16:08:48 +0700

    middlwarecheckadmins2

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::get('test99', function(){
     }
 });
 Route::get('/home', 'HomeController@index');
-Route::get('/admin', 'AdminController@index')->middleware('checkadmin');;
+Route::get('/admin', 'AdminController@index')->middleware('auth')->middleware('checkadmin');
 
 
 
