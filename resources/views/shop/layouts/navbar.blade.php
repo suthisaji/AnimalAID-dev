7334f05e6e3a00efced5baf9b800295015e0bc78
suthisaji <asihtus10@gmail.com>
2017-10-28 22:36:10 +0700

    e

--- resources/views/shop/layouts/navbar.blade.php
+++ resources/views/shop/layouts/navbar.blade.php
@@ -1,3 +1,11 @@
+<style>
+
+
+.navbar-light .navbar-nav .open>.nav-link, .navbar-light .navbar-nav .active>.nav-link, .navbar-light .navbar-nav .nav-link.open, .navbar-light .navbar-nav .nav-link.active {
+    color: #fdce9a;
+}
+
+</style>
 <nav class="navbar navbar-toggleable-md navbar-light bg-inverse">
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -7,11 +15,11 @@
     </a>
     <div class="collapse navbar-collapse" id="navbarColor01">
         <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">หน้าแรก <span class="sr-only">(current)</span></a>
+            <li class="nav-item ">
+                <a class="nav-link" href="all">หน้าแรก <span class="sr-only">(current)</span></a>
             </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">สินค้า</a>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">เลือกสั่งซื้อของที่ระลึก</a>
             </li>
         </ul>
         <ul class="navbar-nav">
